2cf8ea531e9225cc1f47681949f78197f82a8f11
Dilshan <dilshan.fdo@gmail.com>
2019-07-03 09:55:44 +0530

    Update API Keys

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Keep variables and DATA globally
 module.exports = {
     response: '', // to store response DATA
-    privateKey: 'abcd', //Marvel private API Key
-    publicKey: 1234
+    privateKey: 'dc0a1f8f54bc117445ebd6d1dbe720eef05e6160', //Marvel private API Key
+    publicKey: '2ee124a3b2926c6bc894d9847d25bced'
 }
